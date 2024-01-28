c00e8f2ac6ea915f36b5c7505cb8267ca36f8366
Simeon Tran <siatran@ucsc.edu>
2024-01-28 15:02:57 -0800

    Using stdbool

--- components/ot_send/include/cli.h
+++ components/ot_send/include/cli.h
@@ -2,10 +2,11 @@
 #define CLI_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 #include <openthread/cli.h>
 
 #define OT_ARRAY_LENGTH(arr) (sizeof(arr) / sizeof(arr[0]))
-#define OT_SEND_CLI_USE 1
+#define OT_SEND_CLI_USE true
 
 void otCliSentCustomCommands(otInstance *aInstance);
 otError cliIntroString(void *aContext, uint8_t aArgsLength, char *aArgs[]);
