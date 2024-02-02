f18dc8c986c8b6f5edb376220075c5734bccce66
Simeon Tran <siatran@ucsc.edu>
2024-02-01 16:19:11 -0800

    Removing unncessar function

--- components/ot_send/connect.c
+++ components/ot_send/connect.c
@@ -3,14 +3,6 @@
 
 #define NETWORK_KEY "52e83530bb0cebcdbe7e12e6d878b8c6"
 
-void otAutoStart(otInstance *aInstance) {
-  otError error = otThreadSetEnabled(aInstance, true);
-  if (error != OT_ERROR_NONE) {
-    ERROR_PRINT(otLogCritPlat("%s", otThreadErrorToString(error))); 
-  }
-  return;
-}
-
 void checkConnection(otInstance *aInstance) {
   otDeviceRole currentRole;
   do {
